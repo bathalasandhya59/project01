d88ba58b7b4d52f7d3b3d7879266c8ead56aeacc
bathalasandhya59 <bathalasandhya59@gmail.com>
2025-12-12 09:15:16 +0530

    first commit

---
+++ Placements.js
@@ -0,0 +1,9 @@
+const slider = document.getElementById("slider");
+
+function slideLeft() {
+    slider.scrollLeft -= 250;
+}
+
+function slideRight() {
+    slider.scrollLeft += 250;
+}
---
+++ Script.js
@@ -0,0 +1,7 @@
+function scrollLeft() {
+    document.getElementById("scrollBox").scrollLeft -= 300;
+}
+
+function scrollRight() {
+    document.getElementById("scrollBox").scrollLeft += 300;
+}
